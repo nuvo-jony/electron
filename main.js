6aee7355af501cf7d81104890b2ecb0cc1caa566
Jonathan Castells Canales <jonathan.castells@nuvolar.eu>
2023-02-09 15:22:15 +0100

    link angular

---
+++ main.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            // preload: path.join(__dirname, 'preload.js')
+        }
+    });
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, `/dist/index.html`),
+        protocol: "file:",
+        slashes: true
+    }));
+};
+
+app.whenReady().then(() => {
+    createWindow()
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+});
+
+app.whenReady().then(() => {
+    createWindow()
+  
+    app.on('activate', () => {
+      if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+});
+
